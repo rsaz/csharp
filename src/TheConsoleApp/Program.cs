cfac01a2514c067a3afb0dbdc16c23b76159a450
Richard Zampieri <br.developer@gmail.com>
2019-10-06 21:04:58 -0700

    Initial commit

---
+++ src/TheConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Project: Console Application Anatomy
+ * Author: Richard Zampieri
+ * Email: br.developer@gmail.com
+ */
+
+// Namespaces or libraries that provide functionalities out-of-the-box.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace of this file called: TheConsoleApp.
+namespace TheConsoleApp
+{
+    // Class definition called Program.
+    class Program
+    {
+        // Static method called "Main" that doesn't return any value.
+        // string[] args = it is a string array of arguments that can be passed to the application
+        // via command line.
+        static void Main(string[] args)
+        { // brackets or curly braces define the code scope
+
+            // Implement here your application . . .
+        }
+    }
+}
